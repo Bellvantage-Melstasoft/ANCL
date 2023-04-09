7ffbd3b758ee33a86a2b3d816d9fa39effd1e160
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-04-09 22:14:42 +0530

    AddItemPOReports class LIbrary Created

---
+++ CLibrary/Infrastructure/AddItemPOReportsDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLibrary.Infrastructure
+{
+    public interface AddItemPOReportsDAO
+    {
+    }
+    public class AddItemPOReportsDAOSqlImpl : AddItemPOReportsDAO
+    {
+
+    }
+}
