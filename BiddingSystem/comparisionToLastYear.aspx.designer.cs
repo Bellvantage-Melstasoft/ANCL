24ee6b78e08c92d75be240dbc406bb4cc5a02dee
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2023-04-11 15:30:16 +0530

    Comparision To Last Year PO  Table Done

--- BiddingSystem/comparisionToLastYear.aspx.designer.cs
+++ BiddingSystem/comparisionToLastYear.aspx.designer.cs
@@ -60,6 +60,15 @@ namespace BiddingSystem
         protected global::System.Web.UI.WebControls.DropDownList ddlStatus;
 
         /// <summary>
+        /// tblTaSummary control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblTaSummary;
+
+        /// <summary>
         /// gvSupplierItemReport control.
         /// </summary>
         /// <remarks>
